fc37090a6faf1a2f5ab7cc6c36103b34c61cdb73
Alan Alvarado <alanalvara@hotmail.com>
2026-05-12 17:13:18 -0500

    Found out how to make the slider horizontal.

--- src/tweening_maya_tool.py
+++ src/tweening_maya_tool.py
@@ -9,33 +9,23 @@ def get_maya_main_win():
     return wrapInstance(int(main_win_addr), QtWidgets.QWidget)
 
 
-class TweeningToolWindow(QtWidgets.QWidget):
+class TweeningToolWindow(QtWidgets.QDialog):
 
     def __init__(self):
         super().__init__(parent=get_maya_main_win())
-        #self.bookShelf = Bookshelf()
         self.setWindowTitle("Tweening Tool")
         self.resize(300, 200)
         self._mk_main_layout()
 
-    def build_bookshelf(self):
-        # Frame Options
-        self.bookShelf.overall_height = self.shelf_height_dspnx.value()
-        self.bookShelf.shelf_width = self.shelf_width_dspnx.value()
-        self.bookShelf.shelf_depth = self.shelf_depth_dspnx.value()
-        # Book Options
-        self.bookShelf.shelf_levels = self.shelf_levels_spnx.value()
-        self.bookShelf.books_offset = self.books_offset_dspnx.value()
-        self.bookShelf.generate_bookshelf()
-
     def _mk_main_layout(self):
         self.main_layout = QtWidgets.QVBoxLayout(self)
         self._mk_tween_slider_ui()
-        self.setLayout(self.main_layout)
 
     def _mk_tween_slider_ui(self):
-        self.slider = QtWidgets.QSlider()
+        self.slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
         self.slider.setMinimum(0)
         self.slider.setMaximum(100)
         self.slider.setValue(50)
         self.main_layout.addWidget(self.slider)
+
+
